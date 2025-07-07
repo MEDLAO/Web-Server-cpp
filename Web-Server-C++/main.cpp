f3a8fb2c498cbbc36a8cd9def708b6497bfe9dc2
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-07-07 16:38:50 +0100

    Create initial TCP socket with basic error handling

--- Web-Server-C++/main.cpp
+++ Web-Server-C++/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
+#include <sys/socket.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Step 1: Create a socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    // Step 2: Check if the socket was created successfully
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket. \n";
+        return 1;
+    }
+    
+    std::cout << "Socket created successfully. File descriptor = " << server_fd << "\n";
+    
     return 0;
 }
