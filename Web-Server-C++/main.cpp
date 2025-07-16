8582d303c2d9fc15db64667f2f99570d7573ad83
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-07-16 18:09:15 +0100

    Add HTTP response and send it to client

--- Web-Server-C++/main.cpp
+++ Web-Server-C++/main.cpp
@@ -49,15 +49,9 @@ int main(int argc, const char * argv[]) {
     
     std::cout << "Server is listening on port 8080... \n";
     
-    // Keep the server running so we can test it
-    std::cout << "Press Enter to stop the server... \n";
-    std::cin.get(); // Wait forn the user to press Enter
-    
     // Step 6: Accepting incoming requests
     sockaddr_in client_addr;
-    
     socklen_t client_len = sizeof(client_addr);
-    
     int client_socket = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
     
     char buffer[1024] = {0};
@@ -70,7 +64,20 @@ int main(int argc, const char * argv[]) {
         std::cout << "Received from client: " << buffer << "\n";  // Otherwise, print the received data
     }
     
+    
+    // Step 8: Send a simple HTTP response
+    const char* http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from C++ server!";
+    send(client_socket, http_response, strlen(http_response), 0);
+    
+    // Close the client socket after responding
+    close(client_socket);
+    
+    // Close the server socket before exiting
     close(server_fd);
+    
+    // Keep the server running so we can test it
+    std::cout << "Press Enter to stop the server... \n";
+    std::cin.get(); // Wait forn the user to press Enter
     
     return 0;
 }
